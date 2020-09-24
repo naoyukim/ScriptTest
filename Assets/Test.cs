16e59794de6071762adae682b6454cac5c5138b0
naoyukim <70514181+naoyukim@users.noreply.github.com>
2020-09-24 16:19:48 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //要素数５の配列を初期化する
+        int[] array = new int[5];
+
+        //配列の各要素に値を代入する
+        array[0] = 30;
+        array[1] = 20;
+        array[2] = 50;
+        array[3] = 10;
+        array[4] = 80;
+        //配列の要素をすべて表示する
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+        array[0] = 30;
+        array[1] = 20;
+        array[2] = 50;
+        array[3] = 10;
+        array[4] = 80;
+        //配列の要素を逆順に表示する
+        for(int i = 4; i < 5; i--)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
